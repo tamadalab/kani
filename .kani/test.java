1be837c1c867985dc925bc8c34d3a4a396ca8f5c
ma-sa321 <attmptmine@gmail.com>
2020-08-14 08:10:20 +0900

    add test.java

---
+++ .kani/test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+  public static void main(String[] args) {
+    int sum = 0;
+    for (int i = 1; i <= 5; i++) {
+      sum += i;//変数sumにiを足す
+    }
+    System.out.println(sum);//変数sumを表示
+  }
+
+}

bfcea6a2d1a2d1231ba403e69d91ddf8055737e8
ma-sa321 <attmptmine@gmail.com>
2020-10-20 09:37:48 +0900

    fix analyses_link

--- analyses/analyses.py
+++ analyses/analyses.py
@@ -9,7 +9,7 @@ PATH = '/usr/local/opt/kani/analyses/'
 def search_analyses_file():
   # commitすべき状態か判断する．
 
-  with open('{PATH}diff_lines.csv') as data:
+  with open(f'{PATH}diff_lines.csv') as data:
     lines = csv.reader(data)
     criteria = 5 # 判定基準
     flag = 0 # 基準を超えたものが有る場合，commitにいついての表示を出すため1にする．
@@ -17,9 +17,9 @@ def search_analyses_file():
       total_line = int(line[0])+int(line[1])
       if total_line > criteria:
         show_message(line[2],total_line,criteria)
-        index = 1
+        flag = 1
 
-    if index == 1:
+    if flag == 1:
       f = open(f'{PATH}guide_commit.txt','r',encoding='UTF-8')
       data = f.read()
       print(data)

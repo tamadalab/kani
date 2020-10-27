1784bab732767d0e411a612925f5498703a0878e
masa <36505606+ma-sa321@users.noreply.github.com>
2020-10-27 16:53:41 +0900

    fix .kani2 name

--- scripts/precmd_hook.sh
+++ scripts/precmd_hook.sh
@@ -5,7 +5,7 @@ PROJECT_DIR=$($KANI_HOME/scripts/find-project-dir.sh)
 FAILURES_DIR=$PROJECT_DIR/.kani2/failures_compilation
 
 function find_previous_command() {
-    prev_cmd=$PROJECT_DIR/.kani2/prev_cmd
+    prev_cmd=$PROJECT_DIR/.kani/prev_cmd
     if [[ -f $prev_cmd ]]; then
         cat $prev_cmd # 内容を確認する．
         rm $prev_cmd  # 読み出し後，削除する．

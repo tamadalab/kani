dd43154d206c54f8a5a02bbfb1970bb908e040d7
ma-sa321 <attmptmine@gmail.com>
2020-10-29 08:29:55 +0900

    fix outputlog&ccommenst

--- scripts/precmd_hook.sh
+++ scripts/precmd_hook.sh
@@ -23,6 +23,7 @@ else
     if [[ $prevcmd =~ gcc* || $prevcmd =~ clang* && $1 -ne 0 ]]; then # 終了ステータスが正常0以外の時，回数をカウントする．
       # hogeの所gccに変更する．
       echo "$prevcmd : $1" >> $FAILURES_DIR # 失敗回数をカウントするように記述していく．
+      echo "error : status $1" >> $PROJECT_DIR/.kani/test.log 
     elif [[ $prevcmd =~ gcc* || $prevcmd =~ clang* && $1 -eq 0 && -e $FAILURES_DIR ]]; then # エラーが直った場合，連続エラー回数を記録してファイルを削除
       count=$(wc -l $FAILURES_DIR)
       rm $FAILURES_DIR

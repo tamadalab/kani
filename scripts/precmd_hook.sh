dbd8bdb59114899425d4856a1b12db515ec07006
ma-sa321 <attmptmine@gmail.com>
2020-10-28 10:10:04 +0900

    fix rm prev_cmd

--- scripts/precmd_hook.sh
+++ scripts/precmd_hook.sh
@@ -6,8 +6,8 @@ FAILURES_DIR=$PROJECT_DIR/.kani/failures_compilation
 
 function find_previous_command() {
     prev_cmd=$PROJECT_DIR/.kani/prev_cmd
-    if [[ -f $prev_cmd ]]; then
-        cat $prev_cmd # 内容を確認する．
+    if [[ -e $prev_cmd ]]; then
+        # cat $prev_cmd # 内容を確認する．
         rm $prev_cmd  # 読み出し後，削除する．
     fi
 }
